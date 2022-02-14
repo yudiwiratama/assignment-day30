ee3114357fed0b63c77cddcce3cb8489595cc0fb
yudiwiratama <97220605+yudiwiratama@users.noreply.github.com>
2022-02-14 23:14:16 +0700

    Update select.js

--- mysql/select.js
+++ mysql/select.js
@@ -1,1 +1,8 @@
+const db = require('../config/db');
+
+let selectQuery = `SELECT * FROM petShop`;
+db.query(selectQuery, function (error, results, fields) {
+    if (error) throw error;
+    console.log('List petShop: ', results);
+});
 
