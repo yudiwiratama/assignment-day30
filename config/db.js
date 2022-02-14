1c9d03b838625bcc2f35d2b4752a602ca22c17f8
yudiwiratama <97220605+yudiwiratama@users.noreply.github.com>
2022-02-14 23:22:56 +0700

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,1 +1,9 @@
+var mysql      = require('mysql');
+var db = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : '1223',
+  database : 'latihanday30'
+});
 
+module.exports = db
