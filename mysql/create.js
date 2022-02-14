63cb49a31d8c263a1285fdcf66d0d14b60b47740
yudiwiratama <97220605+yudiwiratama@users.noreply.github.com>
2022-02-14 23:00:23 +0700

    Update create.js

--- mysql/create.js
+++ mysql/create.js
@@ -1,1 +1,23 @@
+const db = require("../config/db");
 
+let createQuery = `
+CREATE TABLE IF NOT EXISTS petShop (
+    id INT NOT NULL AUTO_INCREMENT,
+    petId INT(10) NOT NULL, 
+    quantity INT(10) NOT NULL, 
+    status VARCHAR(12) NOT NULL,
+    completed VARCHAR(10) NOT NULL,
+    createdAt DATETIME NULL DEFAULT CURRENT_TIMESTAMP(),
+    updatedAt DATETIME NULL DEFAULT CURRENT_TIMESTAMP(),
+    PRIMARY KEY (id)
+)
+COLLATE='utf8mb4_general_ci'
+;
+`;
+
+db.query(createQuery, function (error, results, fields) {
+  if (error) throw error;
+  console.log("Table has been created");
+});
+
+db.end();
