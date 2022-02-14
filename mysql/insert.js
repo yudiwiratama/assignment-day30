9f01571e16a25a6ca0d48cd0b28be1b4c752e91d
yudiwiratama <97220605+yudiwiratama@users.noreply.github.com>
2022-02-14 23:13:15 +0700

    Update insert.js

--- mysql/insert.js
+++ mysql/insert.js
@@ -1,1 +1,19 @@
+const db = require('../config/db');
+
+let data = [
+    {petId: 1, quantity: 29, status: "placed", completed: "true"},
+	{petId: 2, quantity: 30, status: "available", completed: "true"},
+	{petId: 3, quantity: 42, status: "sold", completed: "true"}
+];
+
+data.forEach (value => {
+    let insertQuery = `INSERT INTO petShop (petId, quantity, status, completed) VALUES (?);`;
+    const {petId, quantity, status, completed} = value;
+    db.query(insertQuery, {petId, quantity, status, completed} , function (error, results, fields) {
+        if (error) throw error;
+        console.log('Data has been inserted');
+    });
+});
+
+
 
