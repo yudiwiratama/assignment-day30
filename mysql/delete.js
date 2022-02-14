c6cb55a361b5e1dfac03f3d171badba20cf1b4be
yudiwiratama <97220605+yudiwiratama@users.noreply.github.com>
2022-02-14 23:00:51 +0700

    Update delete.js

--- mysql/delete.js
+++ mysql/delete.js
@@ -1,1 +1,8 @@
+const db = require('../config/db') 
 
+let deleteQuery = `DELETE FROM petShop WHERE id = ?`
+let data = [4]
+db.query(deleteQuery, data, function (err, deleted) {
+  if (err) throw err;
+  console.log(deleted);
+});
