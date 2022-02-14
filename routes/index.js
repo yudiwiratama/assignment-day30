94fb256f133ab20b918e7e6268b023fcd497f123
yudiwiratama <97220605+yudiwiratama@users.noreply.github.com>
2022-02-14 22:58:25 +0700

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,1 +1,14 @@
+const express = require('express');
+const petStoreController = require('../controller/petShop.controller');
+const router = express.Router();
 
+router.get('/', (req, res) => {
+    res.json({message: 'Hi, welcome !'});
+});
+
+router.get('/petShop/', petStoreController.listPetStore);
+router.post('/petShop/', petStoreController.insertPetStore);
+router.put('/petShop/:id', petStoreController.updatePetStore);
+router.delete('/petShop/:id', petStoreController.deletePetStore);
+
+module.exports = router;    
