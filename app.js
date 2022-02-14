58ee0acbcb441e0fe6e36b03477eb4062b821a81
yudiwiratama <97220605+yudiwiratama@users.noreply.github.com>
2022-02-14 22:53:16 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
+const express = require('express')
+const indexRouter = require('./routes/index')
 
+const app = express()
+const port = 3000
+
+app.use(express.json())
+app.use('/', indexRouter)
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
