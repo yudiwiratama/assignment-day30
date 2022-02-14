66dca7ac93bf78c878f8a686ffb96d0542c65745
yudiwiratama <97220605+yudiwiratama@users.noreply.github.com>
2022-02-14 23:14:47 +0700

    Update update.js

--- mysql/update.js
+++ mysql/update.js
@@ -1,1 +1,9 @@
+const db = require('../config/db');
 
+let updateQuery = `UPDATE petShop SET petId =?, quantity =?, status=?, completed=? WHERE id = ?`;
+let data = [1, "100", "available", "true", 1];
+
+db.query(updateQuery, data, function (err, updated) {
+    if (err) throw error;
+    console.log(updated);
+});
